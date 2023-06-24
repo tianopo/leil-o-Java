c5e489876371b69e89a5bfd7f4ae966fd29ef93a
Matheus Henrique de Abreu <matheuslink18@hotmail.com>
2023-06-24 09:11:56 -0300

    inicio do projeto

---
+++ src/conectaDAO.java
@@ -0,0 +1,22 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class conectaDAO {
+    
+    public Connection connectDB(){
+        Connection conn = null;
+        
+        try {
+        
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/leilão","root","4queijos.");
+            
+        } catch (SQLException erro){
+            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+        }
+        return conn;
+    }
+    
+}
