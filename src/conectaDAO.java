54377b520b1a85fd8291ee43bd114fc32dc81903
Matheus Henrique de Abreu <matheuslink18@hotmail.com>
2023-07-01 09:06:35 -0300

    conexão e desconexão com o banco de dados

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -1,22 +1,37 @@
 
+import com.mysql.jdbc.PreparedStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 public class conectaDAO {
+    Connection con;
+    PreparedStatement st;
+    ResultSet rs;
     
-    public Connection connectDB(){
-        Connection conn = null;
-        
+    private final String url = "jdbc:mysql://localhost:3306/leilão";
+    private final String user = "root";
+    private final String password = "4queijos.";
+    
+    public Connection connectDB(){ 
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/leilão","root","4queijos.");
-            
+            con = DriverManager.getConnection(url, user, password);     
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
         }
-        return conn;
+        return con;
     }
     
+    public void desconectar(){
+        try{
+            if(con != null && !con.isClosed()){
+                con.close();
+                System.out.println("Desconectou do Banco");
+            }
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Não foi possível se desconectar");
+        }
+    }
 }
